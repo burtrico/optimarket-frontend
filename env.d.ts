8b2fb94372613850c797be499b9ba50f22a69b04
Jack Winfield <platocrat@platocrats-MacBook-Pro.local>
2021-11-22 11:26:50 -0800

    walletconnect w/Optimism

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+export declare namespace NodeJS {
+  interface ProcessEnv {
+    INFURA_ETHEREUM_MAINNET_URL: string,
+    INFURA_ETHEREUM_KOVAN_URL: string,
+    INFURA_OPTIMISM_MAINNET_URL: string,
+    INFURA_OPTIMISM_KOVAN_URL: string,
+    METAMASK_WALLET_PRIVATE_KEY_1: string,
+    METAMASK_WALLET_PRIVATE_KEY_2: string,
+    NODE_ENV: 'development' | 'production'
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,144 +1,152 @@
-import * as React from "react";
-import styled from "styled-components";
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "@walletconnect/qrcode-modal";
-import { convertUtf8ToHex } from "@walletconnect/utils";
-import { IInternalEvent } from "@walletconnect/types";
-import Button from "./components/Button";
-import Column from "./components/Column";
-import Wrapper from "./components/Wrapper";
-import Modal from "./components/Modal";
-import Header from "./components/Header";
-import Loader from "./components/Loader";
-import { fonts } from "./styles";
-import { apiGetAccountAssets, apiGetGasPrices, apiGetAccountNonce } from "./helpers/api";
+/* External Imports */
+import * as React from "react"
+import styled from "styled-components"
+import WalletConnect from "@walletconnect/client"
+import QRCodeModal from "@walletconnect/qrcode-modal"
+import { convertUtf8ToHex } from "@walletconnect/utils"
+import { IInternalEvent } from "@walletconnect/types"
+
+/* Internal Imports */
+import Button from "./components/Button"
+import Column from "./components/Column"
+import Wrapper from "./components/Wrapper"
+import Modal from "./components/Modal"
+import Header from "./components/Header"
+import Loader from "./components/Loader"
+import Banner from "./components/Banner"
+import AccountAssets from "./components/AccountAssets"
+
+/* Helpers */
+import { apiGetAccountAssets, apiGetGasPrices, apiGetAccountNonce } from "./helpers/api"
 import {
   sanitizeHex,
   verifySignature,
   hashTypedDataMessage,
   hashMessage,
-} from "./helpers/utilities";
-import { convertAmountToRawNumber, convertStringToHex } from "./helpers/bignumber";
-import { IAssetData } from "./helpers/types";
-import Banner from "./components/Banner";
-import AccountAssets from "./components/AccountAssets";
-import { eip712 } from "./helpers/eip712";
+} from "./helpers/utilities"
+import { convertAmountToRawNumber, convertStringToHex } from "./helpers/bignumber"
+import { IAssetData } from "./helpers/types"
+import { eip712 } from "./helpers/eip712"
+
+/* Styles */
+import { fonts } from "./styles"
+
 
 const SLayout = styled.div`
-  position: relative;
-  width: 100%;
-  /* height: 100%; */
-  min-height: 100vh;
-  text-align: center;
-`;
+  position: relative
+  width: 100%
+  /* height: 100% */
+  min-height: 100vh
+  text-align: center
+`
 
 const SContent = styled(Wrapper as any)`
-  width: 100%;
-  height: 100%;
-  padding: 0 16px;
-`;
+  width: 100%
+  height: 100%
+  padding: 0 16px
+`
 
 const SLanding = styled(Column as any)`
-  height: 600px;
-`;
+  height: 600px
+`
 
 const SButtonContainer = styled(Column as any)`
-  width: 250px;
-  margin: 50px 0;
-`;
+  width: 250px
+  margin: 50px 0
+`
 
 const SConnectButton = styled(Button as any)`
-  border-radius: 8px;
-  font-size: ${fonts.size.medium};
-  height: 44px;
-  width: 100%;
-  margin: 12px 0;
-`;
+  border-radius: 8px
+  font-size: ${ fonts.size.medium }
+  height: 44px
+  width: 100%
+  margin: 12px 0
+`
 
 const SContainer = styled.div`
-  height: 100%;
-  min-height: 200px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  word-break: break-word;
-`;
+  height: 100%
+  min-height: 200px
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  word-break: break-word
+`
 
 const SModalContainer = styled.div`
-  width: 100%;
-  position: relative;
-  word-wrap: break-word;
-`;
+  width: 100%
+  position: relative
+  word-wrap: break-word
+`
 
 const SModalTitle = styled.div`
-  margin: 1em 0;
-  font-size: 20px;
-  font-weight: 700;
-`;
+  margin: 1em 0
+  font-size: 20px
+  font-weight: 700
+`
 
 const SModalParagraph = styled.p`
-  margin-top: 30px;
-`;
+  margin-top: 30px
+`
 
 // @ts-ignore
 const SBalances = styled(SLanding as any)`
-  height: 100%;
+  height: 100%
   & h3 {
-    padding-top: 30px;
+    padding-top: 30px
   }
-`;
+`
 
 const STable = styled(SContainer as any)`
-  flex-direction: column;
-  text-align: left;
-`;
+  flex-direction: column
+  text-align: left
+`
 
 const SRow = styled.div`
-  width: 100%;
-  display: flex;
-  margin: 6px 0;
-`;
+  width: 100%
+  display: flex
+  margin: 6px 0
+`
 
 const SKey = styled.div`
-  width: 30%;
-  font-weight: 700;
-`;
+  width: 30%
+  font-weight: 700
+`
 
 const SValue = styled.div`
-  width: 70%;
-  font-family: monospace;
-`;
+  width: 70%
+  font-family: monospace
+`
 
 const STestButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-`;
+  width: 100%
+  display: flex
+  justify-content: center
+  align-items: center
+  flex-wrap: wrap
+`
 
 const STestButton = styled(Button as any)`
-  border-radius: 8px;
-  font-size: ${fonts.size.medium};
-  height: 44px;
-  width: 100%;
-  max-width: 175px;
-  margin: 12px;
-`;
+  border-radius: 8px
+  font-size: ${ fonts.size.medium }
+  height: 44px
+  width: 100%
+  max-width: 175px
+  margin: 12px
+`
 
 interface IAppState {
-  connector: WalletConnect | null;
-  fetching: boolean;
-  connected: boolean;
-  chainId: number;
-  showModal: boolean;
-  pendingRequest: boolean;
-  uri: string;
-  accounts: string[];
-  address: string;
-  result: any | null;
-  assets: IAssetData[];
+  connector: WalletConnect | null
+  fetching: boolean
+  connected: boolean
+  chainId: number
+  showModal: boolean
+  pendingRequest: boolean
+  uri: string
+  accounts: string[]
+  address: string
+  result: any | null
+  assets: IAssetData[]
 }
 
 const INITIAL_STATE: IAppState = {
@@ -153,166 +161,168 @@ const INITIAL_STATE: IAppState = {
   address: "",
   result: null,
   assets: [],
-};
+}
+
+
 
 class App extends React.Component<any, any> {
   public state: IAppState = {
     ...INITIAL_STATE,
-  };
+  }
 
   public connect = async () => {
     // bridge url
-    const bridge = "https://bridge.walletconnect.org";
+    const bridge = "https://bridge.walletconnect.org"
 
     // create new connector
-    const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal });
+    const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal })
 
-    await this.setState({ connector });
+    await this.setState({ connector })
 
     // check if already connected
     if (!connector.connected) {
       // create new session
-      await connector.createSession();
+      await connector.createSession()
     }
 
     // subscribe to events
-    await this.subscribeToEvents();
-  };
+    await this.subscribeToEvents()
+  }
   public subscribeToEvents = () => {
-    const { connector } = this.state;
+    const { connector } = this.state
 
     if (!connector) {
-      return;
+      return
     }
 
     connector.on("session_update", async (error, payload) => {
-      console.log(`connector.on("session_update")`);
+      console.log(`connector.on("session_update")`)
 
       if (error) {
-        throw error;
+        throw error
       }
 
-      const { chainId, accounts } = payload.params[0];
-      this.onSessionUpdate(accounts, chainId);
-    });
+      const { chainId, accounts } = payload.params[0]
+      this.onSessionUpdate(accounts, chainId)
+    })
 
     connector.on("connect", (error, payload) => {
-      console.log(`connector.on("connect")`);
+      console.log(`connector.on("connect")`)
 
       if (error) {
-        throw error;
+        throw error
       }
 
-      this.onConnect(payload);
-    });
+      this.onConnect(payload)
+    })
 
     connector.on("disconnect", (error, payload) => {
-      console.log(`connector.on("disconnect")`);
+      console.log(`connector.on("disconnect")`)
 
       if (error) {
-        throw error;
+        throw error
       }
 
-      this.onDisconnect();
-    });
+      this.onDisconnect()
+    })
 
     if (connector.connected) {
-      const { chainId, accounts } = connector;
-      const address = accounts[0];
+      const { chainId, accounts } = connector
+      const address = accounts[0]
       this.setState({
         connected: true,
         chainId,
         accounts,
         address,
-      });
-      this.onSessionUpdate(accounts, chainId);
+      })
+      this.onSessionUpdate(accounts, chainId)
     }
 
-    this.setState({ connector });
-  };
+    this.setState({ connector })
+  }
 
   public killSession = async () => {
-    const { connector } = this.state;
+    const { connector } = this.state
     if (connector) {
-      connector.killSession();
+      connector.killSession()
     }
-    this.resetApp();
-  };
+    this.resetApp()
+  }
 
   public resetApp = async () => {
-    await this.setState({ ...INITIAL_STATE });
-  };
+    await this.setState({ ...INITIAL_STATE })
+  }
 
   public onConnect = async (payload: IInternalEvent) => {
-    const { chainId, accounts } = payload.params[0];
-    const address = accounts[0];
+    const { chainId, accounts } = payload.params[0]
+    const address = accounts[0]
     await this.setState({
       connected: true,
       chainId,
       accounts,
       address,
-    });
-    this.getAccountAssets();
-  };
+    })
+    this.getAccountAssets()
+  }
 
   public onDisconnect = async () => {
-    this.resetApp();
-  };
+    this.resetApp()
+  }
 
   public onSessionUpdate = async (accounts: string[], chainId: number) => {
-    const address = accounts[0];
-    await this.setState({ chainId, accounts, address });
-    await this.getAccountAssets();
-  };
+    const address = accounts[0]
+    await this.setState({ chainId, accounts, address })
+    await this.getAccountAssets()
+  }
 
   public getAccountAssets = async () => {
-    const { address, chainId } = this.state;
-    this.setState({ fetching: true });
+    const { address, chainId } = this.state
+    this.setState({ fetching: true })
     try {
       // get account balances
-      const assets = await apiGetAccountAssets(address, chainId);
+      const assets = await apiGetAccountAssets(address, chainId)
 
-      await this.setState({ fetching: false, address, assets });
+      await this.setState({ fetching: false, address, assets })
     } catch (error) {
-      console.error(error);
-      await this.setState({ fetching: false });
+      console.error(error)
+      await this.setState({ fetching: false })
     }
-  };
+  }
 
-  public toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  public toggleModal = () => this.setState({ showModal: !this.state.showModal })
 
-  public testSendTransaction = async () => {
-    const { connector, address, chainId } = this.state;
+  public sendTransaction = async () => {
+    const { connector, address, chainId } = this.state
 
     if (!connector) {
-      return;
+      return
     }
 
     // from
-    const from = address;
+    const from = address
 
     // to
-    const to = address;
+    const to = address
 
     // nonce
-    const _nonce = await apiGetAccountNonce(address, chainId);
-    const nonce = sanitizeHex(convertStringToHex(_nonce));
+    const _nonce = await apiGetAccountNonce(address, chainId)
+    const nonce = sanitizeHex(convertStringToHex(_nonce))
 
     // gasPrice
-    const gasPrices = await apiGetGasPrices();
-    const _gasPrice = gasPrices.slow.price;
-    const gasPrice = sanitizeHex(convertStringToHex(convertAmountToRawNumber(_gasPrice, 9)));
+    const gasPrices = await apiGetGasPrices()
+    const _gasPrice = gasPrices.slow.price
+    const gasPrice = sanitizeHex(convertStringToHex(convertAmountToRawNumber(_gasPrice, 9)))
 
     // gasLimit
-    const _gasLimit = 21000;
-    const gasLimit = sanitizeHex(convertStringToHex(_gasLimit));
+    const _gasLimit = 21000
+    const gasLimit = sanitizeHex(convertStringToHex(_gasLimit))
 
     // value
-    const _value = 0;
-    const value = sanitizeHex(convertStringToHex(_value));
+    const _value = 0
+    const value = sanitizeHex(convertStringToHex(_value))
 
     // data
-    const data = "0x";
+    const data = "0x"
 
     // test transaction
     const tx = {
@@ -323,17 +333,17 @@ class App extends React.Component<any, any> {
       gasLimit,
       value,
       data,
-    };
+    }
 
     try {
       // open modal
-      this.toggleModal();
+      this.toggleModal()
 
       // toggle pending request indicator
-      this.setState({ pendingRequest: true });
+      this.setState({ pendingRequest: true })
 
       // send transaction
-      const result = await connector.sendTransaction(tx);
+      const result = await connector.sendTransaction(tx)
 
       // format displayed result
       const formattedResult = {
@@ -342,49 +352,49 @@ class App extends React.Component<any, any> {
         from: address,
         to: address,
         value: "0 ETH",
-      };
+      }
 
       // display result
       this.setState({
         connector,
         pendingRequest: false,
         result: formattedResult || null,
-      });
+      })
     } catch (error) {
-      console.error(error);
-      this.setState({ connector, pendingRequest: false, result: null });
+      console.error(error)
+      this.setState({ connector, pendingRequest: false, result: null })
     }
-  };
+  }
 
-  public testSignMessage = async () => {
-    const { connector, address, chainId } = this.state;
+  public signMessage = async () => {
+    const { connector, address, chainId } = this.state
 
     if (!connector) {
-      return;
+      return
     }
 
     // test message
-    const message = `My email is john@doe.com - ${new Date().toUTCString()}`;
+    const message = `My email is john@doe.com - ${ new Date().toUTCString() }`
 
     // encode message (hex)
-    const hexMsg = convertUtf8ToHex(message);
+    const hexMsg = convertUtf8ToHex(message)
 
     // eth_sign params
-    const msgParams = [address, hexMsg];
+    const msgParams = [address, hexMsg]
 
     try {
       // open modal
-      this.toggleModal();
+      this.toggleModal()
 
       // toggle pending request indicator
-      this.setState({ pendingRequest: true });
+      this.setState({ pendingRequest: true })
 
       // send message
-      const result = await connector.signMessage(msgParams);
+      const result = await connector.signMessage(msgParams)
 
       // verify signature
-      const hash = hashMessage(message);
-      const valid = await verifySignature(address, result, hash, chainId);
+      const hash = hashMessage(message)
+      const valid = await verifySignature(address, result, hash, chainId)
 
       // format displayed result
       const formattedResult = {
@@ -392,45 +402,45 @@ class App extends React.Component<any, any> {
         address,
         valid,
         result,
-      };
+      }
 
       // display result
       this.setState({
         connector,
         pendingRequest: false,
         result: formattedResult || null,
-      });
+      })
     } catch (error) {
-      console.error(error);
-      this.setState({ connector, pendingRequest: false, result: null });
+      console.error(error)
+      this.setState({ connector, pendingRequest: false, result: null })
     }
-  };
+  }
 
-  public testSignTypedData = async () => {
-    const { connector, address, chainId } = this.state;
+  public signTypedData = async () => {
+    const { connector, address, chainId } = this.state
 
     if (!connector) {
-      return;
+      return
     }
 
-    const message = JSON.stringify(eip712.example);
+    const message = JSON.stringify(eip712.example)
 
     // eth_signTypedData params
-    const msgParams = [address, message];
+    const msgParams = [address, message]
 
     try {
       // open modal
-      this.toggleModal();
+      this.toggleModal()
 
       // toggle pending request indicator
-      this.setState({ pendingRequest: true });
+      this.setState({ pendingRequest: true })
 
       // sign typed data
-      const result = await connector.signTypedData(msgParams);
+      const result = await connector.signTypedData(msgParams)
 
       // verify signature
-      const hash = hashTypedDataMessage(message);
-      const valid = await verifySignature(address, result, hash, chainId);
+      const hash = hashTypedDataMessage(message)
+      const valid = await verifySignature(address, result, hash, chainId)
 
       // format displayed result
       const formattedResult = {
@@ -438,19 +448,19 @@ class App extends React.Component<any, any> {
         address,
         valid,
         result,
-      };
+      }
 
       // display result
       this.setState({
         connector,
         pendingRequest: false,
         result: formattedResult || null,
-      });
+      })
     } catch (error) {
-      console.error(error);
-      this.setState({ connector, pendingRequest: false, result: null });
+      console.error(error)
+      this.setState({ connector, pendingRequest: false, result: null })
     }
-  };
+  }
 
   public render = () => {
     const {
@@ -462,7 +472,7 @@ class App extends React.Component<any, any> {
       showModal,
       pendingRequest,
       result,
-    } = this.state;
+    } = this.state
     return (
       <SLayout>
         <Column maxWidth={1000} spanHeight>
@@ -478,7 +488,7 @@ class App extends React.Component<any, any> {
                 <h3>
                   {`Try out WalletConnect`}
                   <br />
-                  <span>{`v${process.env.REACT_APP_VERSION}`}</span>
+                  <span>{`v${ process.env.REACT_APP_VERSION }`}</span>
                 </h3>
                 <SButtonContainer>
                   <SConnectButton left onClick={this.connect} fetching={fetching}>
@@ -492,15 +502,15 @@ class App extends React.Component<any, any> {
                 <h3>Actions</h3>
                 <Column center>
                   <STestButtonContainer>
-                    <STestButton left onClick={this.testSendTransaction}>
+                    <STestButton left onClick={this.sendTransaction}>
                       {"eth_sendTransaction"}
                     </STestButton>
 
-                    <STestButton left onClick={this.testSignMessage}>
+                    <STestButton left onClick={this.signMessage}>
                       {"eth_sign"}
                     </STestButton>
 
-                    <STestButton left onClick={this.testSignTypedData}>
+                    <STestButton left onClick={this.signTypedData}>
                       {"eth_signTypedData"}
                     </STestButton>
                   </STestButtonContainer>
@@ -547,8 +557,8 @@ class App extends React.Component<any, any> {
           )}
         </Modal>
       </SLayout>
-    );
-  };
+    )
+  }
 }
 
-export default App;
+export default App
